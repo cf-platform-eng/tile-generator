73e724569f4d8c1a9921f11d6020fc738cdea001
guidowb <guidowb@gmail.com>
2016-01-30 05:49:49 -0600

    Debug history

--- ci/scripts/tile-build.sh
+++ ci/scripts/tile-build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/sh
 
 SOURCE_DIR=$1
 HISTORY_DIR=$2
@@ -12,7 +12,9 @@ cd ${BASE_DIR}
 
 if [ -f "${HISTORY_DIR}/tile-history.yml" ]; then
 	cp ${HISTORY_DIR}/tile-history.yml ${SOURCE_DIR}
+	cat ${SOURCE_DIR}/tile-history.yml
 fi
 (cd ${SOURCE_DIR}; ${REPO_DIR}/tile build)
 cp ${SOURCE_DIR}/product/*.pivotal ${TARGET_DIR}
 cp ${SOURCE_DIR}/tile-history.yml ${TARGET_DIR}
+cat ${TARGET_DIR}/tile-history.yml
