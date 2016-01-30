b6a056d3637e22a068e7f9694a924fbaaacf2bd7
guidowb <guidowb@gmail.com>
2016-01-29 20:01:13 -0600

    Corrected paths

--- ci/scripts/tile-build.sh
+++ ci/scripts/tile-build.sh
@@ -5,10 +5,5 @@ REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
 BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 
 cd ${BASE_DIR}
-cd $1
-${REPO_DIR}/tile build
-ls -ld $1
-ls -ld $1/product/*.pivotal
-ls -ld $2
+(cd $1;  ${REPO_DIR}/tile build)
 cp $1/product/*.pivotal $2
-ls -l $2
