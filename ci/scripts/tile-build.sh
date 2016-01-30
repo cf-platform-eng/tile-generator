fa27b174801f4cc798d9a0fca2f4c7cbe43db577
guidowb <guidowb@gmail.com>
2016-01-30 04:35:31 -0600

    Preserve history

--- ci/scripts/tile-build.sh
+++ ci/scripts/tile-build.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+SOURCE_DIR=$1
+HISTORY_DIR=$2
+TARGET_DIR=$3
+
 MY_DIR="$( cd "$( dirname "$0" )" && pwd )"
 REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
 BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 
 cd ${BASE_DIR}
-(cd $1;  ${REPO_DIR}/tile build)
-cp $1/product/*.pivotal $2
-ls -ld $2
-ls -l $2
+
+cp ${HISTORY_DIR}/tile-history.yml ${SOURCE_DIR}
+(cd ${SOURCE_DIR}; ${REPO_DIR}/tile build)
+cp $${SOURCE_DIR}/product/*.pivotal ${TARGET_DIR}
+cp ${SOURCE_DIR}/tile-history.yml ${HISTORY_DIR}
