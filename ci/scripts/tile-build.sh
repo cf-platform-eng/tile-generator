2bc42887048e23c4859d437c0b37bb537cc3a60d
guidowb <guidowb@gmail.com>
2016-01-31 08:10:33 -0600

    Version history

--- ci/scripts/tile-build.sh
+++ ci/scripts/tile-build.sh
@@ -10,11 +10,17 @@ BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 
 cd ${BASE_DIR}
 
-if [ -f "${HISTORY_DIR}/tile-history.yml" ]; then
-	cp ${HISTORY_DIR}/tile-history.yml ${SOURCE_DIR}
-	cat ${SOURCE_DIR}/tile-history.yml
+HISTORY=`ls ${HISTORY_DIR}/tile-history-*.yml`
+if [ -n "${HISTORY}"]; then
+	echo "using history ${HISTORY}"
+	cp ${HISTORY} ${SOURCE_DIR}/tile-history.yml
 fi
+
 (cd ${SOURCE_DIR}; ${REPO_DIR}/tile build)
+
+VERSION=`grep '^version:' ${SOURCE_DIR}/tile-history.yml | sed 's/^version: //'`
+HISTORY="tile-history-${VERSION}.yml"
+echo "saving history ${HISTORY}"
+
 cp ${SOURCE_DIR}/product/*.pivotal ${TARGET_DIR}
-cp ${SOURCE_DIR}/tile-history.yml ${TARGET_DIR}
-cat ${TARGET_DIR}/tile-history.yml
+cp ${SOURCE_DIR}/tile-history.yml ${TARGET_DIR}/tile-history-${VERSION}.yml
