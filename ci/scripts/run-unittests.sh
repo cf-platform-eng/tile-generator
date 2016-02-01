ebd12c034b1c3c9a6138ccaa3617619b674ee567
guidowb <guidowb@gmail.com>
2016-02-01 15:25:26 -0600

    Moved task implementation to script

---
+++ ci/scripts/run-unittests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+UNITTEST_DIR=$1
+
+MY_DIR="$( cd "$( dirname "$0" )" && pwd )"
+REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
+BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
+
+cd ${REPO_DIR}
+
+python -m unittest discover -v -s ${UNITTEST_DIR} -p '*_unittest.py'
