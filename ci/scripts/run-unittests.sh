591b62eb1f3ec3d3ee0bd9cc80dc9709289f65a2
guidowb <guidowb@gmail.com>
2016-02-01 15:30:35 -0600

    Execute from correct directory

--- ci/scripts/run-unittests.sh
+++ ci/scripts/run-unittests.sh
@@ -6,6 +6,6 @@ MY_DIR="$( cd "$( dirname "$0" )" && pwd )"
 REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
 BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 
-cd ${REPO_DIR}
+cd ${BASE_DIR}
 
 python -m unittest discover -v -s ${UNITTEST_DIR} -p '*_unittest.py'
