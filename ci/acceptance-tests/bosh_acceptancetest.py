c497f77b8d397aa4aad97f64ea6b39b1b1b3f48b
guidowb <guidowb@gmail.com>
2016-02-02 17:35:07 -0600

    More tests

--- ci/acceptance-tests/bosh_acceptancetest.py
+++ ci/acceptance-tests/bosh_acceptancetest.py
@@ -55,9 +55,29 @@ class VerifyBoshRelease(unittest.TestCase):
 			if manifest.startswith('release/jobs/docker-bosh-'):
 				self.assertFalse('cf_cli' in read_yaml(manifest).get('packages', []), manifest)
 
+	def test_all_jobs_have_template(self):
+		self.assertEqual(len(glob.glob('release/jobs/*/templates/*.sh.erb')), len(glob.glob('release/jobs/*')))
+
+	def test_has_correct_number_of_packages(self):
+		self.assertEqual(len(glob.glob('release/packages/*')), 13)
+
+	def test_has_complete_cf_cli_package(self):
+		self.assertEqual(len(glob.glob('release/packages/cf_cli')), 1)
+		self.assertEqual(len(glob.glob('release/packages/cf_cli/cf_cli/cf-linux-amd64.tgz')), 1)
+		self.assertEqual(len(glob.glob('release/packages/cf_cli/packaging')), 1)
+		self.assertTrue('cf-linux-amd64.tgz' in read_file('release/packages/cf_cli/packaging'))
+
+	def test_has_complete_common_package(self):
+		self.assertEqual(len(glob.glob('release/packages/common')), 1)
+		self.assertEqual(len(glob.glob('release/packages/common/common/utils.sh')), 1)
+
 def read_yaml(filename):
 	with open(filename, 'rb') as file:
 		return yaml.safe_load(file)
+
+def read_file(filename):
+	with open(filename, 'rb') as file:
+		return file.read()
 
 if __name__ == '__main__':
 	unittest.main()
