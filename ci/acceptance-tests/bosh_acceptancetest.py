1f41a3b9dd6da2e2df901e983fee6477a3c84dcf
guidowb <guidowb@gmail.com>
2016-02-02 09:36:43 -0600

    First passing acceptance test

--- ci/acceptance-tests/bosh_acceptancetest.py
+++ ci/acceptance-tests/bosh_acceptancetest.py
@@ -1,11 +1,12 @@
 import unittest
 import sys
 import os
+import zipfile
 
 class VerifyBoshRelease(unittest.TestCase):
 
-	def test_has_valid_config_final(self):
-		self.assertTrue(os.path.exists('release/config/final.yml'))
+	def test_has_manifest(self):
+		self.assertTrue(os.path.exists('release/release.MF'))
 
 if __name__ == '__main__':
 	unittest.main()
--- ci/scripts/run-acceptancetests.sh
+++ ci/scripts/run-acceptancetests.sh
@@ -8,4 +8,29 @@ BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 TEST_DIR="$( cd "${REPO_DIR}/ci/acceptance-tests" && pwd )"
 
 cd ${TILE_DIR}
+
+TILE_FILE=`ls *.pivotal`
+if [ -z "${TILE_FILE}" ]; then
+	echo "No match for *.pivotal"
+	exit 1
+fi
+
+mkdir product && (cd product && unzip "../${TILE_FILE}")
+
+BOSH_FILE=`ls product/releases/test-tile-*.tgz`
+if [ -z "${BOSH_FILE}" ]; then
+	echo "No match for releases/test-tile-*.tgz"
+	exit 1
+fi
+
+mkdir release && (cd release && tar xvf "../${BOSH_FILE}")
+
+for TGZ_FILE in release/*/*.tgz
+do
+	TGZ_DIR=`echo "${TGZ_FILE}" | sed "s/\.tgz\$//"`
+	mkdir -p "${TGZ_DIR}"
+	(cd "${TGZ_DIR}"; tar xvf "../../../${TGZ_FILE}")
+	rm "${TGZ_FILE}"
+done
+
 python -m unittest discover -v -s ${TEST_DIR} -p '*_acceptancetest.py'
