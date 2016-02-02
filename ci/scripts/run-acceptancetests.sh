c03dd016da20a39f0500b1ec0486ab208b7ed994
guidowb <guidowb@gmail.com>
2016-02-02 08:24:05 -0600

    Acceptance test framework

---
+++ ci/scripts/run-acceptancetests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+TILE_DIR=$1
+
+MY_DIR="$( cd "$( dirname "$0" )" && pwd )"
+REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
+BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
+
+cd ${BASE_DIR}
+
+echo "To Be Implemented"
