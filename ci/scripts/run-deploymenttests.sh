062cd9234a4a0228d764e85cbf1162c00a48cff9
guidowb <guidowb@gmail.com>
2016-02-03 17:04:56 -0600

    Automatically upload and install tile

--- ci/scripts/run-deploymenttests.sh
+++ ci/scripts/run-deploymenttests.sh
@@ -8,19 +8,25 @@ REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
 BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 TEST_DIR="$( cd "${REPO_DIR}/ci/deployment-tests" && pwd )"
 
-TILE_FILE=`ls "${TILE_DIR}"/*.pivotal`
+TILE_FILE=`cd "${TILE_DIR}"; ls *.pivotal`
 if [ -z "${TILE_FILE}" ]; then
 	echo "No files matching ${TILE_DIR}/*.pivotal"
 	ls -lR "${TILE_DIR}"
 	exit 1
 fi
 
+PRODUCT=`echo "${TILE_FILE}" | sed "s/-[^-]*$//"`
+VERSION=`echo "${TILE_FILE}" | sed "s/.*-//" | sed "s/\.pivotal\$//"`
+
 cd "${POOL_DIR}"
 
-echo "available products:"
+echo "Available products:"
 python "${TEST_DIR}/pcf" products
 echo
 
-echo "uploading ${TILE_FILE}"
-python "${TEST_DIR}/pcf" import "${TILE_FILE}"
+echo "Uploading ${TILE_FILE}"
+python "${TEST_DIR}/pcf" import "${TILE_DIR}/${TILE_FILE}"
 echo
+
+echo "Installing product ${PRODUCT} version ${VERSION}"
+python "${TEST_DIR}/pcf" install "${PRODUCT}" "${VERSION}"
