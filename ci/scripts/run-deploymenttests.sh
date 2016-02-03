0650a5656aa1b2ec81471c9525538e7a96b380d1
guidowb <guidowb@gmail.com>
2016-02-03 15:57:36 -0600

    D**n relative paths

--- ci/scripts/run-deploymenttests.sh
+++ ci/scripts/run-deploymenttests.sh
@@ -8,9 +8,12 @@ REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
 BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 TEST_DIR="$( cd "${REPO_DIR}/ci/deployment-tests" && pwd )"
 
+cd "${BASE_DIR}"
+
 TILE_FILE=`cd "${TILE_DIR}"; ls *.pivotal`
 if [ -z "${FILE_FILE}" ]; then
 	echo "No files matching ${TILE_DIR}/*.pivotal"
+	ls -lR "${TILE_DIR}"
 	exit 1
 fi
 
@@ -20,3 +23,6 @@ echo "available products:"
 python "${TEST_DIR}/pcf" products
 echo
 
+echo "uploading ${TILE_FILE}"
+python "${TEST_DIR}/pcf" import "${TILE_DIR}/${TILE_FILE}"
+echo
