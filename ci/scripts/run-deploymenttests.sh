42702c1fefd663aa93b660cf046f9f1ca049b62f
guidowb <guidowb@gmail.com>
2016-02-03 11:36:45 -0600

    First attempt to talk to dynamically reserved opsmgr

--- ci/scripts/run-deploymenttests.sh
+++ ci/scripts/run-deploymenttests.sh
@@ -1,17 +1,12 @@
 #!/bin/sh -e
 
 TILE_DIR=$1
-PCF_RESERVATION=$2
+POOL_DIR=$2
 
 MY_DIR="$( cd "$( dirname "$0" )" && pwd )"
 REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
 BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 TEST_DIR="$( cd "${REPO_DIR}/ci/deployment-tests" && pwd )"
 
-echo "name:"
-echo
-cat $2/name
-echo
-echo "metadata:"
-echo
-cat $2/metadata
+cd "${POOL_DIR}"
+python "${TEST_DIR}/pcf" products
