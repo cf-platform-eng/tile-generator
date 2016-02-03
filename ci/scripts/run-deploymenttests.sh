654b471d1175f6e10e9ebb972e60ec78fd0de2ed
guidowb <guidowb@gmail.com>
2016-02-03 15:38:24 -0600

    Upload generated tile

--- ci/scripts/run-deploymenttests.sh
+++ ci/scripts/run-deploymenttests.sh
@@ -8,5 +8,15 @@ REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
 BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
 TEST_DIR="$( cd "${REPO_DIR}/ci/deployment-tests" && pwd )"
 
+TILE_FILE=`cd "${TILE_DIR}"; ls *.pivotal`
+if [ -z "${FILE_FILE}" ]; then
+	echo "No files matching ${TILE_DIR}/*.pivotal"
+	exit 1
+fi
+
 cd "${POOL_DIR}"
+
+echo "available products:"
 python "${TEST_DIR}/pcf" products
+echo
+
