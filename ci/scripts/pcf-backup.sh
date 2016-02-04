8500b08b699d80327a1e83e697742aff00e3289a
guidowb <guidowb@gmail.com>
2016-02-04 11:00:30 -0600

    Backup settings before deploying tile

---
+++ ci/scripts/pcf-backup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+POOL_DIR="$( cd "$1" && pwd )"
+BACKUP_DIR="$( cd "$2" && pwd )"
+
+MY_DIR="$( cd "$( dirname "$0" )" && pwd )"
+REPO_DIR="$( cd "${MY_DIR}/../.." && pwd )"
+BASE_DIR="$( cd "${REPO_DIR}/.." && pwd )"
+TEST_DIR="$( cd "${REPO_DIR}/ci/deployment-tests" && pwd )"
+
+PCF_NAME=`cat "${POOL_DIR}/name"`
+if [ -z "${PCF_NAME}" ]; then
+	echo "No pcf environment has been claimed"
+	exit 1
+fi
+
+BACKUP_FILE="pcf-backup-${PCF_NAME}-0.0.0.yml"
+
+echo "Saving settings into ${BACKUP_FILE}"
+python "${TEST_DIR}/pcf" backup "${BACKUP_DIR}/${BACKUP_FILE}"
+echo
