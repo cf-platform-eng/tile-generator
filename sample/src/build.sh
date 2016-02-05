b46382d139b5d2ac7c5eb9d6fa9b0c8878b17581
guidowb <guidowb@gmail.com>
2016-02-05 10:58:05 -0600

    Separate target from source for Concourse's sake

--- sample/src/build.sh
+++ sample/src/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -e
 
+BUILD_DIR="$1"
 MY_DIR="$( cd "$( dirname "$0" )" && pwd )"
-SRC_DIR="$( cd "${MY_DIR}" && pwd )"
+SAMPLE_DIR="$( cd "${MY_DIR}/.." && pwd )"
+
+if [ -n "${BUILD_DIR}" ]; then
+	cp -r "${SAMPLE_DIR}"/* "${BUILD_DIR}"
+	SAMPLE_DIR="${BUILD_DIR}"
+fi
+
+SRC_DIR="$( cd "${SAMPLE_DIR}/src" && pwd )"
 APP_DIR="$( cd "${SRC_DIR}/app" && pwd )"
 BUILDPACK_DIR="$( cd "${SRC_DIR}/buildpack" && pwd )"
-RESOURCES_DIR="$( cd "${SRC_DIR}/../resources" && pwd )"
+RESOURCES_DIR="$( cd "${SAMPLE_DIR}/resources" && pwd )"
 
 cd "${APP_DIR}"
 mkdir -p vendor
