6acb354fff4aafcdc006a13d947721d7dcfff5c5
guidowb <guidowb@gmail.com>
2016-02-08 10:33:06 -0600

    Return env as dict rather than array of properties

--- sample/src/app/app.py
+++ sample/src/app/app.py
@@ -9,10 +9,7 @@ app = Flask(__name__)
 
 @app.route("/env")
 def environment():
-	properties = []
-	for k,v in sorted(os.environ.iteritems()):
-		properties.append(k + "=" + v)
-	return json.dumps(properties, indent=4)
+	return json.dumps(dict(os.environ), indent=4)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=int(os.getenv('PORT', '8080')))
