7150d106ebff2c9ca6b5b995a537d50e5fee1f15
guidowb <guidowb@gmail.com>
2016-03-01 18:26:46 -0600

    More deployment tests

--- ci/deployment-tests/app3_deploymenttest.py
+++ ci/deployment-tests/app3_deploymenttest.py
@@ -8,11 +8,11 @@ PATH = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(os.path.join(os.path.join(PATH, '..'), '..'), 'lib'))
 import opsmgr
 
-class VerifyApp2(unittest.TestCase):
+class VerifyApp3(unittest.TestCase):
 
 	def setUp(self):
 		self.cfinfo = opsmgr.get_cfinfo()
-		self.hostname = 'app2-hostname.' + self.cfinfo['apps_domain']
+		self.hostname = 'app3.' + self.cfinfo['apps_domain']
 		self.url = 'http://' + self.hostname
 
 	def test_responds_to_hello(self):
@@ -41,7 +41,7 @@ class VerifyApp2(unittest.TestCase):
 		broker1_service = vcap_services.get('service-broker1-service', None)
 		self.assertTrue(broker1_service is not None)
 		self.assertEquals(len(broker1_service), 1)
-		self.assertEquals(broker1_service[0].get('plan'), 'first-plan')
+		self.assertEquals(broker1_service[0].get('plan'), 'second-plan')
 	
 	def test_has_versioned_name(self):
 		headers = { 'Accept': 'application/json' }
@@ -50,7 +50,7 @@ class VerifyApp2(unittest.TestCase):
 		env = response.json()
 		vcap_application = json.loads(env.get('VCAP_APPLICATION'))
 		name = vcap_application.get('application_name')
-		self.assertTrue(name.startswith('app2-'))
+		self.assertTrue(name.startswith('app3-'))
 
 	def test_is_in_correct_space(self):
 		headers = { 'Accept': 'application/json' }
@@ -61,7 +61,7 @@ class VerifyApp2(unittest.TestCase):
 		space= vcap_application.get('space_name')
 		self.assertEquals(space, 'test-tile-space')
 
-	def test_receives_expected_admin_credentials(self):
+	def test_does_not_receive_admin_credentials(self):
 		headers = { 'Accept': 'application/json' }
 		response = requests.get(self.url + '/env', headers=headers)
 		response.raise_for_status()
@@ -69,9 +69,9 @@ class VerifyApp2(unittest.TestCase):
 		user = env.get('CF_ADMIN_USER')
 		username = env.get('CF_ADMIN_USERNAME')
 		password = env.get('CF_ADMIN_PASSWORD')
-		self.assertEquals(user, username)
-		self.assertEquals(user, 'system_services')
-		self.assertFalse(password is None)
+		self.assertTrue(user is None)
+		self.assertTrue(username is None)
+		self.assertTrue(password is None)
 
 if __name__ == '__main__':
 	unittest.main()
