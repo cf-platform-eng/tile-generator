1d7e65a78969e96cf577fa24f255cf8c1f38f9d4
Michael William Boldt <mike.boldt@gmail.com>
2017-04-10 15:30:43 -0500

    Clean up pcf import progress bar.
    Fix the 100% marker.
    Make it look good for smaller tiles.

--- tile_generator/opsmgr.py
+++ tile_generator/opsmgr.py
@@ -183,18 +183,16 @@ class ProgressBar:
 		sys.stdout.flush()
 
 	def update(self, monitor):
+		self.update_every = min(self.update_every, monitor.len / 30)
 		if monitor.bytes_read - self.last_update >= self.update_every:
 			sys.stdout.write('.')
-			if monitor.bytes_read == monitor.len:
-				sys.stdout.write('100%')
-			else:
-				old_percent = float(self.last_update) / monitor.len
-				new_percent = float(monitor.bytes_read) / monitor.len
-				for step in range(90, 0, -10):
-					step /= 100.0
-					if new_percent >= step > old_percent:
-						sys.stdout.write('{:.0%}'.format(step))
-						break
+			old_percent = float(self.last_update) / monitor.len
+			new_percent = float(monitor.bytes_read) / monitor.len
+			for step in range(90, 0, -10):
+				step /= 100.0
+				if new_percent >= step > old_percent:
+					sys.stdout.write('{:.0%}'.format(step))
+					break
 			sys.stdout.flush()
 			self.last_update = monitor.bytes_read
 
@@ -213,7 +211,7 @@ def upload(url, filename, check=True):
 		data=multipart,
 		headers={ 'Content-Type': multipart.content_type }
 	)
-	sys.stdout.write('\n')
+	sys.stdout.write('.100%\n')
 	sys.stdout.flush()
 	if response.status_code == 422:
 		errors = response.json()["errors"]
