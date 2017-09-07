155ffa37592e0d6968a5a1fc6eecff0c19a94747
Michael William Boldt <mike.boldt@gmail.com>
2017-09-07 11:36:40 -0500

    Add constraints to all resource definitions.
    Fixes #197.

--- ci/acceptance-tests/tile_acceptancetest.py
+++ ci/acceptance-tests/tile_acceptancetest.py
@@ -33,39 +33,61 @@ class VerifyTile(unittest.TestCase):
 		self.assertEqual(len(files), 1)
 		read_yaml(files[0]) # Ensure corrent yaml syntax
 
+	def test_contains_tile_yml(self):
+		self.assertTrue(os.path.exists('product/tile-generator'))
+		files = glob.glob('product/tile-generator/tile.yml')
+		self.assertEqual(len(files), 1)
+		read_yaml(files[0])
+
 class VerifyProperties(unittest.TestCase):
 
 	def setUp(self):
 		self.assertTrue(os.path.exists('product/metadata'))
 		files = glob.glob('product/metadata/*.yml')
 		self.assertEqual(len(files), 1)
-		self.config = read_yaml(files[0])
+		self.metadata = read_yaml(files[0])
 
 	def test_optional(self):
-		blueprints = self.config['property_blueprints']
+		blueprints = self.metadata['property_blueprints']
 		self.assertFalse(find_by_name(blueprints, 'author')['optional'])
 		self.assertTrue(find_by_name(blueprints, 'customer_name')['optional'])
 		self.assertFalse(find_by_name(blueprints, 'street_address')['optional'])
 
 	def test_bosh_release_has_properties(self):
-		job = find_by_name(self.config['job_types'], 'redis_leader_z1')
+		job = find_by_name(self.metadata['job_types'], 'redis_leader_z1')
 		self.assertIn('author', job['manifest'])
 
 	def test_default_internet_connected(self):
-		job = find_by_name(self.config['job_types'], 'redis_leader_z1')
+		job = find_by_name(self.metadata['job_types'], 'redis_leader_z1')
 		self.assertIn('default_internet_connected', job)
 		self.assertFalse(job['default_internet_connected'])
 
 	def test_run_errand_default(self):
-		errand = find_by_name(self.config['post_deploy_errands'], 'acceptance-tests')
+		errand = find_by_name(self.metadata['post_deploy_errands'], 'acceptance-tests')
 		self.assertEqual(errand['run_post_deploy_errand_default'], 'when-changed')
 
 	def test_bosh_release_properties_merged(self):
-		job = find_by_name(self.config['job_types'], 'acceptance-tests')
+		job = find_by_name(self.metadata['job_types'], 'acceptance-tests')
 		manifest = yaml.safe_load(job['manifest'])
 		cf = manifest['cf']
 		self.assertIn('some', cf) # Property defined in tile.yml.
 		self.assertIn('admin_user', cf) # Auto-included property.
+
+class VerifyConstraints(unittest.TestCase):
+
+	def setUp(self):
+		self.assertTrue(os.path.exists('product/metadata'))
+		files = glob.glob('product/metadata/*.yml')
+		self.assertEqual(len(files), 1)
+		self.metadata = read_yaml(files[0])
+
+	def test_resource_constraints(self):
+		job = find_by_name(self.metadata['job_types'], 'acceptance-tests')
+		resource_defs = job['resource_definitions']
+		self.assertEqual(find_by_name(resource_defs, 'cpu')['constraints']['min'], 2)
+		self.assertEqual(find_by_name(resource_defs, 'ephemeral_disk')['constraints']['min'], 4096)
+		self.assertEqual(find_by_name(resource_defs, 'persistent_disk')['constraints']['min'], 0)
+		self.assertEqual(find_by_name(resource_defs, 'ram')['constraints']['min'], 512)
 
 def find_by_name(lst, name):
 	return next(x for x in lst if x.get('name', None) == name)
