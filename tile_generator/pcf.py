645e18fe59b37786b4f9cdb1ed0a898682dca9e2
Krsna Widmer <keshto@gmail.com>
2017-10-17 10:57:21 -0700

    Clean up SSH code and automate bosh login

--- tile_generator/pcf.py
+++ tile_generator/pcf.py
@@ -418,14 +418,10 @@ def credentials_cmd():
 def bosh_env_cmd():
 	director_creds = opsmgr.get('/api/v0/deployed/director/credentials/director_credentials').json()
 	director_manifest = opsmgr.get('/api/v0/deployed/director/manifest').json()
-	print('1. Run `pcf -t <target> ssh` to log in to the Ops Manager VM.')
-	print('2. Set these variables:')
-	print('export BOSH_ENVIRONMENT="{}"'.format(director_manifest['jobs'][0]['properties']['director']['address']))
-	print('export BOSH_CA_CERT="/var/tempest/workspaces/default/root_ca_certificate"')
-	print('to target the BOSH environment.')
-	print('3. Run `bosh2 login` with the following username and password:')
-	print('username: {}'.format(director_creds['credential']['value']['identity']))
-	print('password: {}'.format(director_creds['credential']['value']['password']))
+	print('BOSH_ENVIRONMENT="{}"'.format(director_manifest['jobs'][0]['properties']['director']['address']))
+	print('BOSH_CA_CERT="/var/tempest/workspaces/default/root_ca_certificate"')
+	print('BOSH USERNAME={}'.format(director_creds['credential']['value']['identity']))
+	print('BOSH PASSWORD={}'.format(director_creds['credential']['value']['password']))
 
 @cli.command('history')
 def history_cmd():
