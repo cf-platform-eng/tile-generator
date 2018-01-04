2bd4e07cf29c683676105558be1b03f87553fbfc
Krsna Mahapatra <keshto@gmail.com>
2018-01-04 14:30:03 -0800

    Add `is_static` back temporarily for templates

--- tile_generator/package_flags.py
+++ tile_generator/package_flags.py
@@ -44,6 +44,7 @@ class BoshRelease(FlagBase):
         properties = {'name': packagename}
         for job in package.get('jobs', []):
             jobname = job['name']
+            job['is_static'] = job.get('static_ip', 0) > 0
             if job.get('static_ip', 0) > 0:
                 properties.update({
                     job['varname']: {
