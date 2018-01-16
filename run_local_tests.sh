8a3197e2d7a7e9f629aea7d6cf24a5be6b356e8a
Krsna Mahapatra <keshto@gmail.com>
2018-01-16 09:45:11 -0800

    Invert rebuild option for local_tests

--- run_local_tests.sh
+++ run_local_tests.sh
@@ -26,15 +26,18 @@ command -v tile >/dev/null 2>&1 || { echo "Command 'tile' not found." >&2; exit 
 rm -rf "${SCRIPT_DIR}"/"${LOCAL_TEST_DIR}"
 mkdir "${SCRIPT_DIR}"/"${LOCAL_TEST_DIR}"
 
-if [ "$1" = "rebuild" ] || [ ! -e "${SCRIPT_DIR}"/sample/product/*.pivotal ]; then
-    "${SCRIPT_DIR}"/ci/scripts/tile-build.sh "${SCRIPT_DIR}"/sample "${SCRIPT_DIR}"/sample "${SCRIPT_DIR}"/"${LOCAL_TEST_DIR}"
-else
-    echo "############################################################"
-    echo "# Using existing .pivotal file in sample/product/ dir.     #"
-    echo "# Use the'rebuild' argument to force a rebuild of the tile #"
-    echo "############################################################"
+if [ "$1" = "norebuild" ] || [ ! -e "${SCRIPT_DIR}"/sample/product/*.pivotal ]; then
+    echo "########################################################"
+    echo "# Using existing .pivotal file in sample/product/ dir. #"
+    echo "########################################################"
     cp ${SCRIPT_DIR}/sample/product/*.pivotal "${SCRIPT_DIR}"/"${LOCAL_TEST_DIR}"
     cp ${SCRIPT_DIR}/sample/tile-history*.yml "${SCRIPT_DIR}"/"${LOCAL_TEST_DIR}"
+else
+    echo "#############################################################"
+    echo "# Creating a new .pivotal file. Use 'norebuild' argument to #"
+    echo "# use the existing .pivotal file in sample/product/ dir.    #"
+    echo "#############################################################"
+    "${SCRIPT_DIR}"/ci/scripts/tile-build.sh "${SCRIPT_DIR}"/sample "${SCRIPT_DIR}"/sample "${SCRIPT_DIR}"/"${LOCAL_TEST_DIR}"
 fi
 
 "${SCRIPT_DIR}"/ci/scripts/run-acceptancetests.sh "${SCRIPT_DIR}"/"${LOCAL_TEST_DIR}"
