d1a42a8d17212d8dd1f00737a982b3d485924f8d
Pete Wall <pwall@pivotal.io>
2018-08-23 13:49:01 -0500

    s/HOOK/HOOKS/

--- install-git-hook.sh
+++ install-git-hook.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-HOOK_DIR="${SCRIPT_DIR}"/.git/hooks
+HOOKS_DIR="${SCRIPT_DIR}"/.git/hooks
 
 echo 'Installing pre-push hook...'
 
-mkdir -p "${HOOK_DIR}"
+mkdir -p "${HOOKS_DIR}"
 echo '#!/bin/bash -e
 remote="$1"
 url="$2"
@@ -14,8 +14,8 @@ if [[ $url = "git@github.com:cf-platform-eng/tile-generator.git" ]]; then
   ./scripts/run_local_tests.sh withcache
 fi
 
-exit 0' > "${HOOK_DIR}"/pre-push
+exit 0' > "${HOOKS_DIR}"/pre-push
 
-chmod +x "${HOOK_DIR}"/pre-push
+chmod +x "${HOOKS_DIR}"/pre-push
 
 echo 'Done!'
