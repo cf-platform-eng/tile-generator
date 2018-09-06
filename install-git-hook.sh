a58b502a38e99813ac0dfd37a83f205ed01c3961
Pete Wall <pete@petewall.net>
2018-09-06 13:47:27 -0500

    Ensure the hooks directory is created before writing to it. (#277)
    * Ensure the hooks directory exists before installing the hook.
    
    * s/HOOK/HOOKS/

--- install-git-hook.sh
+++ install-git-hook.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+HOOKS_DIR="${SCRIPT_DIR}"/.git/hooks
 
 echo 'Installing pre-push hook...'
 
+mkdir -p "${HOOKS_DIR}"
 echo '#!/bin/bash -e
 remote="$1"
 url="$2"
@@ -12,8 +14,8 @@ if [[ $url = "git@github.com:cf-platform-eng/tile-generator.git" ]]; then
   ./scripts/run_local_tests.sh withcache
 fi
 
-exit 0' > "${SCRIPT_DIR}"/.git/hooks/pre-push
+exit 0' > "${HOOKS_DIR}"/pre-push
 
-chmod +x "${SCRIPT_DIR}"/.git/hooks/pre-push
+chmod +x "${HOOKS_DIR}"/pre-push
 
 echo 'Done!'
