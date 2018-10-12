c80a54fede2ed1d8d4259e96ab49fe2dc07c1348
Krsna Mahapatra <keshto@gmail.com>
2018-10-12 13:55:46 -0700

    Make it possible for no-op to be ran as an errand

--- sample/no-op-release/build.sh
+++ sample/no-op-release/build.sh
@@ -3,14 +3,14 @@
 set -ex
 
 echo "Be sure to increment version if re-using an environment"
-version=0.1.3
+version=0.1.4
 
 rm -rf dev_releases/no-op-release
 
 # pcf-1-11 didn't like the bosh release build with bosh2
 # bosh2 version
-#bosh create-release --force --version=$version --tarball=../resources/no-op-release.tgz
+bosh create-release --force --version=$version --tarball=../resources/no-op-release.tgz
 
 # bosh1 version
-bosh create release --force --version=$version --with-tarball
-mv dev_releases/no-op-release/no-op-release-0.1.3.tgz ../resources/no-op-release.tgz
+#bosh create release --force --version=$version --with-tarball
+#mv dev_releases/no-op-release/no-op-release-$version.tgz ../resources/no-op-release.tgz
