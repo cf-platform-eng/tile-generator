dacd02835137a8729d326b0be549b8107ba59c25
Michael William Boldt <mike.boldt@gmail.com>
2018-10-12 20:15:42 -0500

    Remove PCF 1.12 from CI.

--- ci/generate_pipeline_yml.py
+++ ci/generate_pipeline_yml.py
@@ -3,7 +3,7 @@
 import os
 from jinja2 import Template
 
-clusters = ['1_12', '2_0', '2_1', '2_2']
+clusters = ['2_0', '2_1', '2_2']
 # Commenting out this as we only have one example and it breaks
 tiles = [] # [d for d in os.listdir('../examples') if os.path.isdir(os.path.join('../examples', d))]
 
