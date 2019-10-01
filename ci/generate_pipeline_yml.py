1a837e84a129e99f7734fe0ffdc6ff3a239ecc4a
Krsna Mahapatra <keshto@gmail.com>
2019-10-01 11:30:26 -0700

    Remove 2.3 and 2.4 from CI pipeline

--- ci/generate_pipeline_yml.py
+++ ci/generate_pipeline_yml.py
@@ -3,7 +3,7 @@
 import os
 from jinja2 import Template
 
-clusters = ['2_3', '2_4', '2_5', '2_6']
+clusters = ['2_5', '2_6', '2_7']
 # Commenting out this as we only have one example and it breaks
 tiles = [] # [d for d in os.listdir('../examples') if os.path.isdir(os.path.join('../examples', d))]
 
