7f4b9fb1e7f3b8a608bace69e5281fc0c8b82b65
Michael William Boldt <mike.boldt@gmail.com>
2021-05-10 14:42:09 -0500

    Bump the runtime test release version.
    The old one was using python2 which is no longer on the stemcells.

--- sample/runtime-test-release/build.sh
+++ sample/runtime-test-release/build.sh
@@ -3,14 +3,14 @@
 set -ex
 
 echo "Be sure to increment version if re-using an environment"
-version=0.1.5
+version=0.2.0
 
 rm -rf dev_releases/runtime-test-release
 
 # pcf-1-11 didn't like the bosh release build with bosh2
 # bosh2 version
-#bosh create-release --force --version=$version --tarball=../resources/runtime-test-release.tgz
+bosh create-release --force --version=$version --tarball=../resources/runtime-test-release.tgz
 
 # bosh1 version
-bosh create release --force --version=$version --with-tarball
-mv dev_releases/runtime-test-release/runtime-test-release-0.1.5.tgz ../resources/runtime-test-release.tgz
+# bosh create release --force --version=$version --with-tarball
+# mv dev_releases/runtime-test-release/runtime-test-release-0.1.5.tgz ../resources/runtime-test-release.tgz
